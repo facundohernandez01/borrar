cb4916a6e231d7f6fd91d1a0420a06523f8c4a77
Facundo <31361481+facundohernandez01@users.noreply.github.com>
2023-02-10 16:22:31 -0300

    goback

--- src/Checkout/checkout_modal.jsx
+++ src/Checkout/checkout_modal.jsx
@@ -24,7 +24,7 @@ const { cart, vaciarCarrito } = useContext(CartContext);
 const [finalizada, setFinalizada] = useState(false);
 function handleClose() {
         setFinalizada(false);
-        window.history.back();
+        window.history.back(-2);
 }
 
 const [inputNombre, setInputNombre] = useState("");
