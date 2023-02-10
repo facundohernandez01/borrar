bbd2b300751619ff4dd65db14baed42e10085cd5
Facundo <31361481+facundohernandez01@users.noreply.github.com>
2023-02-10 16:44:53 -0300

    LOGO

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -37,9 +37,23 @@ function ResponsiveAppBar({handleClickOpen, open}) {
     >      
 <Container maxWidth="xl">
         <Toolbar disableGutters>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        <Button variant='text' component={RouterLink} to="/">LOGO</Button>
-
+        <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none', lg: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+        LOGO
         </Typography>
 
       <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', lg: 'none'  } }}>
