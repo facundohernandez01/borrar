87f17b7a1182b1a282917754a67997c6724986e9
Facundo <31361481+facundohernandez01@users.noreply.github.com>
2023-02-10 18:40:56 -0300

    404

--- src/Page404.jsx
+++ src/Page404.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Page404 = () => {
   return (
-    <div>Error 404</div>
+    <>
+    <h1>La página solicitada no existe</h1><br></br>
+    <p>Este sitio muestra productos que provienen de Firebase y los ID de los productos son dinámiso.
+      Es posible que si se refresque la página o se vuelva hacía atrás, se reciba un error 404</p>
+    </>
   )
 }
 
