e343c58bbb52fe12dfd896c9e1a9f7c655d0ca31
Facundo <31361481+facundohernandez01@users.noreply.github.com>
2023-02-10 18:45:54 -0300

    404_2

--- src/Page404.jsx
+++ src/Page404.jsx
@@ -6,7 +6,7 @@ const Page404 = () => {
     <h1>La página solicitada no existe</h1><br></br>
     <img src="https://digitea.es/wp-content/uploads/2015/04/404digitea.gif"></img>
     <p>Este sitio muestra productos que provienen de Firebase y los ID de los productos son dinámiso.
-      Es posible que si se refresque la página o se vuelva hacía atrás, se reciba un error 404</p>
+      Es posible que si se refresca la página, o vuelva hacía atrás, se reciba un error 404</p>
     </>
   )
 }
