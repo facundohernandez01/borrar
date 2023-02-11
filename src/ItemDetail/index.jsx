706a589eb1c3e6389dd62866afa530daa35b84f7
Facundo <31361481+facundohernandez01@users.noreply.github.com>
2023-02-11 14:52:08 -0300

    x

--- src/ItemDetail/index.jsx
+++ src/ItemDetail/index.jsx
@@ -52,12 +52,12 @@ const ItemDetail = () => {
     <>
 
 
-  <Grid container spacing={4} sx={{py: '400px'}}>
+  <Grid container spacing={4} sx={{py: 400}}>
       <Grid item xs={12} md={6} lg={7} >
       {loading ? (
       <Skeleton sx={{ height: 290, width: 500, minWidth:500}} animation="wave" variant="rectangular" />
       ) : ( 
-        <Item>
+        <Item sx={{maxWidth: 400}}>
         <img className="itemThumb" src={ItemsList.image_url}></img>
        </Item>
             )}
